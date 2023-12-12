86d3a0f61583b05a8169ae4561a1a39e4f5bae47
Ravi Tokas <ravitokas90@gmail.com>
2023-12-12 15:32:06 +0530

    update chai.sol and deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,41 @@
 const { ethers } = require("hardhat");
 
+async function getBalances(address) {
+  const balanceBigInt = await ethers.provider.getBalance(address);
+  return ethers.utils.formatEther(balanceBigInt);
+}
+
+async function consoleBalances(addresses) {
+  let counter = 0;
+
+  for (const address of addresses) {
+    console.log(`Address ${counter} balance: `, await getBalances(address));
+  }
+}
+
+async function consoleMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+
+    console.log(
+      `At ${timestamp}, name ${name}, address ${from}, message ${message}`
+    );
+  }
+}
+
 async function main() {
-  async function getBalances(address) {
-    const balanceBigInt = await ethers.provider.getBalance(address);
-    return ethers.utils.formatEther(balanceBigInt);
-  }
+  const [owner, from1, from2, from3] = await ethers.getSigners();
+  const contract = await ethers.deployContract("Chai");
 
-  async function consoleBalances(addresses) {
-    let counter = 0;
+  console.log("Address of contract: ", contract.address);
 
-    for (const address of addresses) {
-      console.log(`Address ${counter} balance: `, await getBalances(address));
-    }
-  }
+  const addresses = [owner.address, from1.address];
 
-  async function consoleMemos(memos) {
-    for (const memo of memos) {
-      const timestamp = memo.timestamp;
-      const name = memo.name;
-      const from = memo.from;
-      const message = memo.message;
-
-      console.log(
-        `At ${timestamp}, name ${name}, address ${from}, message ${message}`
-      );
-    }
-  }
+  console.log("before buying chai");
+  await consoleBalances(addresses);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
