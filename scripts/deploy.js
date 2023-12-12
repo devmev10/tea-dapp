c395ae3333bb1d2834df4db76b78122dfc38cce4
Ravi Tokas <ravitokas90@gmail.com>
2023-12-12 15:14:28 +0530

    add consoleMemos function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,6 +13,19 @@ async function main() {
       console.log(`Address ${counter} balance: `, await getBalances(address));
     }
   }
+
+  async function consoleMemos(memos) {
+    for (const memo of memos) {
+      const timestamp = memo.timestamp;
+      const name = memo.name;
+      const from = memo.from;
+      const message = memo.message;
+
+      console.log(
+        `At ${timestamp}, name ${name}, address ${from}, message ${message}`
+      );
+    }
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
