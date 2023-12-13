10ad1604e251ec56bcc7edaf7949e93da74b264a
Ravi Tokas <ravitokas90@gmail.com>
2023-12-13 14:22:47 +0530

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,35 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
 function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x4a5B09EF55Fcef35DB8359ea544EeEe34F45424F";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+        }
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+      } catch (error) {}
+    };
+  }, []);
+
   return <div className="App"></div>;
 }
 
